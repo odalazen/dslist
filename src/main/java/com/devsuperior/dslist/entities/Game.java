e60c605f2e59e9d48fa4f74315b0c2335463f238
Lucas Dalazen <lucasdalazen06@gmail.com>
2025-01-20 22:20:26 -0300

    GameMinDTO, GameRepository, GameService, GameController

--- src/main/java/com/devsuperior/dslist/entities/Game.java
+++ src/main/java/com/devsuperior/dslist/entities/Game.java
@@ -26,8 +26,11 @@ public class Game {
 	private String platforms;
 	private Double score;
 	private String imgUrl;
+	
+	@Column(columnDefinition = "TEXT")
 	private String shortDescription;
-	@Column(length = 500)
+	
+	@Column(columnDefinition = "TEXT")
 	private String longDescription;
 	
 	public Game() {
