4767989de7166479efd4e4a7a372f95f094d8229
Lucas Dalazen <lucasdalazen06@gmail.com>
2025-01-21 21:40:00 -0300

    Consulta SQL, projection, search by list id

--- src/main/java/com/devsuperior/dslist/controllers/GameListController.java
+++ src/main/java/com/devsuperior/dslist/controllers/GameListController.java
@@ -8,27 +8,32 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.devsuperior.dslist.dto.GameDTO;
+import com.devsuperior.dslist.dto.GameListDTO;
 import com.devsuperior.dslist.dto.GameMinDTO;
+import com.devsuperior.dslist.services.GameListService;
 import com.devsuperior.dslist.services.GameService;
 
 @RestController
-@RequestMapping(value = "/games")
+@RequestMapping(value = "/lists")
 public class GameListController {
 
 	@Autowired
+	private GameListService gameListService;
+	
+	@Autowired
 	private GameService gameService;
 	
-	@GetMapping(value = "/{id}")
-	public GameDTO findById(@PathVariable Long id){
-		GameDTO result = gameService.findById(id);
-		return result;
+	@GetMapping
+	public List<GameListDTO> findAll(){
+		return gameListService.findAll();
+		
+		
 	}
 	
-	@GetMapping
-	public List<GameMinDTO> findAll(){
-		return gameService.findAll();
-		
+	@GetMapping(value = "/{listId}/games")
+	public List<GameMinDTO> findByList(@PathVariable Long listId){
+		List<GameMinDTO> result = gameService.findByList(listId);
+		return result;
 		
 	}
 }
